efe4d788805346eeea3fd3fb493c8031a8ed3316
Maxikyo <holovanovmax@gmail.com>
2023-03-13 00:32:03 +0100

    finished controller

--- src/main/groovy/com/holovanovmax/spring_groovy_gradle/controller/Controller.groovy
+++ src/main/groovy/com/holovanovmax/spring_groovy_gradle/controller/Controller.groovy
@@ -2,6 +2,7 @@ package com.holovanovmax.spring_groovy_gradle.controller
 
 import com.holovanovmax.spring_groovy_gradle.dao.UsersRepository
 import com.holovanovmax.spring_groovy_gradle.entity.Users
+import com.holovanovmax.spring_groovy_gradle.request.UsersRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -15,59 +16,83 @@ import org.springframework.web.bind.annotation.RestController
 
 
 //!first variant of controller!
-//@RestController
-//@RequestMapping("api/")
-//    class Controller {
-//
-//    private final UsersRepository usersRepository;
-//
-//   Controller(UsersRepository usersRepository) {
-//        this.usersRepository = usersRepository;
-//    }
-//
-//    @GetMapping("/users")
-//    ResponseEntity<List<Users>> getAllUsers(){
-//        return ResponseEntity.ok(this.usersRepository.findAll());
-//    }
-//
-//    @PostMapping("/users")
-//    ResponseEntity<Users> createUsers(@RequestBody Users users) {
-//        return ResponseEntity.ok(this.usersRepository.save(users))
-//    }
-//
-//}
-
-//!Second variant of controller!
 @RestController
-@RequestMapping("api")
-class Controller {
+    class Controller {
 
-    @Autowired
-    UsersRepository usersService
+    private final UsersRepository usersRepository;
 
-    @GetMapping
-    List<Users> getAllUsers() {
-    usersService.findAll()
+    Controller(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
     }
 
-    @PostMapping
-    Users saveUsers(@RequestBody Users users) {
-        usersService saveUsers(Users)
+    @GetMapping("/users")
+    ResponseEntity<List<Users>> getAllUsers() {
+        return ResponseEntity.ok(this.usersRepository.findAll());
     }
 
-    @PutMapping("/{usersId}")
-    Users updateUsers(PathVariable usersId, @RequestBody Users users){
-        usersService.updateUsers(usersId, users)
+    @PostMapping("/users")
+    ResponseEntity<Users> createProduct(@RequestBody UsersRequest usersRequest) {
+        Users users = new Users();
+
+        return ResponseEntity.status(201).body(this.usersRepository.save(users));
+
     }
 
-    @DeleteMapping("/{usersId}")
-    deleteUsers(@PathVariable usersId){
-        usersService.findAllById(usersId)
+    @GetMapping("/users/{id}")
+    ResponseEntity getProductById(@PathVariable String id) {
+        Optional<Users> product = this.usersRepository.findById(id);
+
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product.get());
+        } else {
+            return ResponseEntity.ok("The product with " + id + " was not found.");
+        }
     }
 
-    @GetMapping("/{usersId}")
-    Users getUsersById (@PathVariable usersId){
-        usersService.findAllById(usersId)
+    @DeleteMapping("/users/{id}")
+    ResponseEntity deleteProductById(@PathVariable String id) {
+        Optional<Users> product = this.usersRepository.findById(id);
+
+        if (product.isPresent()) {
+            this.usersRepository.deleteById(id);
+            return ResponseEntity.ok("Done");
+        } else {
+            return ResponseEntity.ok("The product with " + id + " was not found.");
+        }
     }
 
 }
+////!Second variant of controller!
+//@RestController
+//@RequestMapping("api")
+//class Controller {
+//
+//    @Autowired
+//    UsersRepository usersService
+//
+//    @GetMapping
+//    List<Users> getAllUsers() {
+//    usersService.findAll()
+//    }
+//
+//    @PostMapping
+//    Users saveUsers(@RequestBody Users users) {
+//        usersService saveUsers(Users)
+//    }
+//
+//    @PutMapping("/{usersId}")
+//    Users updateUsers(PathVariable usersId, @RequestBody Users users){
+//        usersService.updateUsers(usersId, users)
+//    }
+//
+//    @DeleteMapping("/{usersId}")
+//    deleteUsers(@PathVariable usersId){
+//        usersService.findAllById(usersId)
+//    }
+//
+//    @GetMapping("/{usersId}")
+//    Users getUsersById (@PathVariable usersId){
+//        usersService.findAllById(usersId)
+//    }
+//
+//}
